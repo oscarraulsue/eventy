9010db0ef2505fe640c7243af76cae8fd7355ddd
Oscar Raul Sue Alvarado <oscar.sue@vasslatam.com>
2023-07-05 09:19:12 -0500

    overflow

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Header } from "@/compoments/Header";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col items-center justify-between overflow-hidden">
       <div className="bg-[rgba(0,0,0,0.917)] flex w-full flex-col items-center justify-between p-4">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <Header />
